084c476f4266bc09cd923df01ff734b29b523830
Sumon <sumonece15@gmail.com>
2021-01-07 16:27:36 +0600

    Radio Button

---
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -0,0 +1,43 @@
+    package com.example.radiobutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private Button showButton;
+    private RadioButton genderButton;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroupId);
+        showButton = (Button) findViewById(R.id.showButtonId);
+        resultTextView = (TextView) findViewById(R.id.resultTextViewId);
+
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+               int selectedId =radioGroup.getCheckedRadioButtonId();
+               genderButton =(RadioButton) findViewById(selectedId);
+
+               String value = genderButton.getText().toString();
+               resultTextView.setText("You have selected  :  "+value);
+            }
+        });
+
+    }
+}
